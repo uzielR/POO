68f31ce62065f8bee8f777fd0868d3a320db7d5a
uzielR <uziiel2a2@gmail.com>
2023-06-14 17:09:28 -0600

    Practica No.5
    comit 5

--- practica_5/a.py
+++ practica_5/a.py
@@ -1,9 +1,27 @@
 from flask import Flask
-app = Flask (__name__)
+from flask_mysqldb import MySQL
 
+#inicializacion del APP
+app = Flask (__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='bdflask'
+mysql = MySQL(app)
+
+#declaracion de la ruta al local host
 @app.route('/')
 def index():
     return "hola mundo Flask"
 
+@app.route('/GUARDAR')
+def GUARDAR():
+    return "se guardo en la BD"
+
+@app.route('/eliminar')
+def eliminar():
+    return "se elimino en la BD"
+  
+#ejecucion del servidor en el puerto 5000
 if __name__ == '__main__':
-  app.run(port=5000)
+  app.run(port=5000,debug=True)
